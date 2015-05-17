7e74e454987b380abeca2dfeaa44047a6bfdc9f2
Steven Taylor Ramzel <steven.ramzel@elementum.com>
2015-05-17 13:33:48 -0700

    Added batch uploading, added AddFoodDesc

--- src/main/java/Fndds2ParseDataLoader.java
+++ src/main/java/Fndds2ParseDataLoader.java
@@ -5,8 +5,10 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.RollingFileAppender;
 import org.parse4j.Parse;
+import org.parse4j.ParseBatch;
 import org.parse4j.ParseException;
 import org.parse4j.ParseObject;
+import org.parse4j.ParseQuery;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,58 +18,106 @@ import java.util.Scanner;
 /**
  * Created by stevenramzel on 4/27/15.
  */
-public class Fndds2ParseDataLoader{
+public class Fndds2ParseDataLoader {
 
-    public static final String CLASS_NAME = Fndds2ParseDataLoader.class.getSimpleName();
     public static final String APPLICATION_ID = "5V6uI4uhzAzzKc4NMIHfyHVjqi6FFBW6eyi3Yy8o";
     public static final String REST_API_KEY = "djPEF0DiqgFz2JqerokYTjX4DuN5e2knm2Rjo8Vv";
     public static final String PATTERN = "%d{dd-MM-yyyy HH:mm:ss} %C %L %-5p:%m%n";
+    public static final String FILE_LOG = "file.log";
+    public static final String[] CLASS_NAMES = new String[]{
+            "MainFoodDesc",
+            "AddFoodDesc"
+    };
 
-    public static void main(String[] args){
-        initLogger();
+    enum TypeFactory {
+        string {
+            @Override
+            Object next(Scanner scanner) {
+                return scanner.next();
+            }
+        },
+        integer {
+            @Override
+            Object next(Scanner scanner) {
+                return scanner.nextLong();
+            }
+        },
+        fraction {
+            @Override
+            Object next(Scanner scanner) {
+                return scanner.nextFloat();
+            }
+        },
+        date {
+            @Override
+            Object next(Scanner scanner) {
+                return scanner.next();
+            }
+        };
+
+        abstract Object next(Scanner scanner);
+    }
+
+    public static void main(String[] args) {
+        Logger logger = initLogger();
 
         Parse.initialize(APPLICATION_ID, REST_API_KEY);
-        System.out.println("Parse Initialized");
+        logger.info("Parse Initialized");
 
-        try {
-            Scanner scanner = getScanner(new FileReader("./assets/mainfooddesc.txt"), 0L);
-            System.out.println("File loaded");
-            ArrayList<String> hashSet = new ArrayList<>();
-            Scanner fieldScanner = new Scanner(scanner.nextLine());
-            fieldScanner.useDelimiter("~?\\^~?");
-            while (fieldScanner.hasNext()){
-                String next = fieldScanner.next();
-                hashSet.add(next);
-                System.out.println("Added Field" + next);
-            }
-            int n = 0;
-            while (scanner.hasNextLine()){
-                ParseObject parseObject = new ParseObject("MainFoodDesc");
-                for (String key : hashSet) {
-                    if (scanner.hasNext()) {
-                        parseObject.put(key, scanner.next());
+        for (String className : CLASS_NAMES) {
+            try {
+                Scanner scanner = getScanner(new FileReader("./assets/" + className + ".txt"), 0L);
+                logger.info("File loaded");
+                ArrayList<Key> keyList = new ArrayList<>();
+                Scanner fieldScanner = new Scanner(scanner.nextLine());
+                fieldScanner.useDelimiter("~?\\^~?");
+                while (fieldScanner.hasNext()) {
+                    String next = fieldScanner.next();
+                    String[] split = next.split(":");
+                    Key key = new Key(split[0], split[1]);
+                    keyList.add(key);
+                    logger.info("Added Field " + next);
+                }
+                int count = ParseQuery.getQuery(className).count();
+                for (int i = 0; i < count; i++) scanner.nextLine();
+                int n = count;
+                ParseBatch batcher = new ParseBatch();
+                while (scanner.hasNextLine()) {
+                    ParseObject parseObject = new ParseObject(className);
+                    for (Key key : keyList) {
+                        Object next = key.next(scanner);
+                        if (!TypeFactory.date.equals(key.mType)) {
+                            parseObject.put(key.mKey, next);
+                        }
+                    }
+                    batcher.createObject(parseObject);
+                    scanner.nextLine();
+                    if (++n%50 == 0) {
+                        logger.info("Added entry " + n);
+                        batcher.batch();
+                        batcher = new ParseBatch();
                     }
                 }
-                System.out.println("Added entry " + ++n);
-                parseObject.save();
+            } catch (IOException | ParseException e) {
+                e.printStackTrace();
+                logger.error(e);
             }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
         }
     }
 
-    private static void initLogger() {
-        Logger logger = Logger.getLogger(CLASS_NAME);
+    private static Logger initLogger() {
+        Logger logger = Logger.getLogger(Fndds2ParseDataLoader.class.getSimpleName());
         BasicConfigurator.configure();
         Logger.getRootLogger().setLevel(Level.INFO);
         FileAppender fileAppender = null;
         try {
             PatternLayout layout = new PatternLayout(PATTERN);
-            fileAppender = new RollingFileAppender(layout,"file.log");
+            fileAppender = new RollingFileAppender(layout, FILE_LOG);
             logger.addAppender(fileAppender);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return logger;
     }
 
     public static Scanner getScanner(FileReader sce, Long skipLines) throws IOException {
@@ -75,5 +125,20 @@ public class Fndds2ParseDataLoader{
         for (long n = 0L; n < skipLines; n++) scanner.nextLine();
         scanner.useDelimiter("~?\\^~?");
         return scanner;
+    }
+
+    private static class Key {
+
+        private final TypeFactory mType;
+        private final String mKey;
+
+        public Key(String key, String type) {
+            mKey = key;
+            mType = TypeFactory.valueOf(type);
+        }
+
+        Object next(Scanner scanner) {
+            return mType.next(scanner);
+        }
     }
 }
